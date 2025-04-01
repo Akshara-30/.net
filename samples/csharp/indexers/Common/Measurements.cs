027e5f9dff813b71df9f9a25ae30d58bcee08f26
Abhijith C V <abhijith.cv@lntinfotech.com>
2025-04-01 16:46:21 +0530

    Batch commit 108

---
+++ samples/csharp/indexers/Common/Measurements.cs
@@ -0,0 +1,11 @@
+﻿namespace IndexersSamples.Common
+ //comments
+{
+    public class Measurements
+    {
+        public int HiTemp { get; set; }
+        public int LoTemp { get; set; }
+
+        public double AirPressure { get; set; }
+    }
+}
